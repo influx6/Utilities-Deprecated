f2f56b572717644884e22383f8495c0d2cf43caf
Alex Ewetumo <trinoxf@gmail.com>
2012-03-31 23:39:23 +0100

    adding main.js and package.json file

---
+++ main.js
@@ -0,0 +1,6 @@
+var nodelib = require('./nodelib');
+var less-watcher = require('./less-watcher');
+var jsConcat = require('./jsConcat');
+
+
+export.Utilities = { nodelib: nodelib, less-watcher:less-watcher, jsConcat:jsConcat};
